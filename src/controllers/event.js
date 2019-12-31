339f65e720f9247f7e42f6ae7144e86d02aa8d01
Ian King <ian.antking@gmail.com>
2019-12-31 10:28:35 +0000

    added exec child process to event controller

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,4 +1,12 @@
+const exec = require('child_process').exec;
+
+const execCallback = (error, stdout, stderr) => {
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+    if (error) console.log(error);
+}
+
 exports.handleEvent = (req, res) => {
-    console.log(req.body);
+    exec("pwd", execCallback)
     res.status(200).json({ message: req.body.message });
 }
