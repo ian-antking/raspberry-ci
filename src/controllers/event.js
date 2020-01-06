e7f800f535e45a05c23c8495539fd63588dce30c
Ian King <ian.antking@gmail.com>
2020-01-06 23:48:36 +0000

    added error response to event controller

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -2,14 +2,17 @@ const exec = require('child_process').exec;
 
 const execCallback = (error, stdout, stderr) => {
     if (stdout) console.log(stdout);
-    if (stderr) console.log(stderr);
+    if (stderr) {
+        console.log(stderr)
+        res.status(500).json({ error: stderr });
+    };
     if (error) console.log(error);
 }
 
-exports.handleEvent = (req, res) => {
+exports.handleEvent = (_, res) => {
     console.log('Pulling code from github...');
     exec(`git -C ${process.env.PROJECT_PATH} reset --hard`, execCallback);
     exec(`git -C ${process.env.PROJECT_PATH} clean -df`, execCallback);
     exec(`git -C ${process.env.PROJECT_PATH} pull -f`, execCallback);
-    res.status(200).json({ message: req.body.message });
+    res.status(200).json({ message: "code successfully integrated" });
 }
