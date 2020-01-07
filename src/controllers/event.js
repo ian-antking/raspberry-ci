744b65aa984660799c71120829a1dfe5bd753639
Ian King <ian.antking@gmail.com>
2020-01-07 00:06:49 +0000

    fixed error handling so that successful integration does not cause crash

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,18 +1,26 @@
 const exec = require('child_process').exec;
 
-const execCallback = (error, stdout, stderr) => {
-    if (stdout) console.log(stdout);
-    if (stderr) {
-        console.log(stderr)
-        res.status(500).json({ error: stderr });
-    };
-    if (error) console.log(error);
-}
-
 exports.handleEvent = (_, res) => {
+    const execCallback = (error, stdout, stderr) => {
+        if (stdout) {
+            res.status(200).json({ message: "code successfully integrated" });
+            console.log(stdout);
+        };
+        if (stderr) {
+            console.log(stderr)
+            res.status(500).json({ error: stderr });
+        };
+        if (error) {
+            console.log(error);
+            res.status(500).json({ error: stderr })
+        };
+    }
+
     console.log('Pulling code from github...');
-    exec(`git -C ${process.env.PROJECT_PATH} reset --hard`, execCallback);
-    exec(`git -C ${process.env.PROJECT_PATH} clean -df`, execCallback);
-    exec(`git -C ${process.env.PROJECT_PATH} pull -f`, execCallback);
-    res.status(200).json({ message: "code successfully integrated" });
+    exec(`
+            git -C ${process.env.PROJECT_PATH} reset --hard &&
+            git -C ${process.env.PROJECT_PATH} clean -df &&
+            git -C ${process.env.PROJECT_PATH} pull -f
+            `, execCallback
+        );
 }
