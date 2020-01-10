7e379cea5d6ad597df2bbd8db955a84dd5e52153
Ian King <ian.antking@gmail.com>
2020-01-10 13:13:55 +0000

    removed console.log from event controller

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -3,7 +3,6 @@ const runCommand = require('../utils/run-command');
 exports.handleEvent = (req, res) => {
     const repo = req.repo;
 
-    console.log('Pulling code from github...');
     if (req.body.dryRun) {
         runCommand(`echo Pulling code in ${repo}`, res);
     } else {
