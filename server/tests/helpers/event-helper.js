537093f5725f3c96da8f071b11024cd6adf8574a
Ian King <ian.antking@gmail.com>
2020-01-13 22:18:03 +0000

    updated event-helper sha

--- server/tests/helpers/event-helper.js
+++ server/tests/helpers/event-helper.js
@@ -1,7 +1,7 @@
 exports.pushEvent = data => new Promise((resolve, reject) => {
   chai.request(server)
   .post('/event')
-  .set('x-hub-signature', 'sha1=bfe5830a33bf4b9e9e2f87c1d3f6f279f4ba4c66')
+  .set('x-hub-signature', 'sha1=027242ab59cc2034825073faaff5fadf993bc536')
   .send(data)
   .end((error, response) => {
     if (error) {
