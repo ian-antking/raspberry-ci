a1bb20ee2a1dff1c1c81d2d167ec87dd1346f287
Ian King <ian.antking@gmail.com>
2020-01-13 21:02:58 +0000

    fixed signature encryption

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,12 +1,15 @@
 const crypto = require('crypto');
 
 const auth = (req, res, next) => {
+   console.log(req.body.toString());
     const sha = crypto
-    .createHmac('sha1', process.env.SECRET)
-    .update(req.body.toString())
-    .digest('hex');
+        .createHmac('sha1', process.env.SECRET)
+        .update(JSON.stringify(req.body))
+        .digest('hex');
     const sig = `sha1=${sha}`
-    
+
+    console.log(sig);
+
     if (req.headers['x-hub-signature'] !== sig) {
         res.status(401).json({ error: 'permission denied' })
     } else {
